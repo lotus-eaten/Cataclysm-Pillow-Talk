90f9df6d870d7eeb691e50d582f8a073c67f8793
lotus-eaten <melikeroblox8101@gmail.com>
2026-09-15 10:01:57 -0700

    Update prompts.lua

--- prompts.lua
+++ prompts.lua
@@ -6,5 +6,20 @@ return {
     "The plushie is surprisingly springy.  It quickly returns to its original shape after being squeezed.",
     "You examine the plushie closely.  Its surface is covered in soft fabric, with the characteristic appearance of a slime creature.",
     "You squeeze the plushie for a moment.  It's soft, yielding, and oddly satisfying to hold."
-  }
+  },
+    sword_plushie = {
+    "There's no weight and no heft to it, and yet somehow, you feel more confident wielding it.", 
+    "*Shkriiiip!* Despite the prehensile nature of the tassel, the scabbard comes off as you pull stiff velcro hooks from soft loops. As if second nature, you place the blade within the scabbard. What terrific product quality!",
+    "You hallucinate the tassel trying to wrap around your wrist. What you don't hallucinate is the blue eye looking directly at you a moment after.",
+    "Your fingers run over the plushie's surface. Soft and fuzzy, and yet somehow it feels almost... durable.",
+    "The fabric is smooth to the touch, with a little bit of give when you squeeze it.",
+    "A few swings confirms, to your surprise, a surprising stiffness to. The tell-tale floppy nature of a pillow blade is nowhere to be seen. *Swish, swish, stab!*",
+  },
+    sword_slumber = {
+    "There's no weight and no heft to it, and yet somehow, you feel more confident wielding it.", 
+    "*Shkriiiip!* Despite the prehensile nature of the tassel, the scabbard comes off as you pull stiff velcro hooks from soft loops. As if second nature, you place the blade within the scabbard. What terrific product quality!",
+    "You hallucinate the tassel trying to wrap around your wrist. What you don't hallucinate is the blue eye looking directly at you a moment after.",
+    "Your fingers run over the plushie's surface. Soft and fuzzy, and yet somehow it feels almost... durable.",
+    "The fabric is smooth to the touch, with a little bit of give when you squeeze it.",
+    "A few swings confirms, to your surprise, a surprising stiffness to. The tell-tale floppy nature of a pillow blade is nowhere to be seen. *Swish, swish, stab!*",
 }
