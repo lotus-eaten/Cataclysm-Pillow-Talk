2810b30f1629e8f2eb8d77575a16bd3c4b6b3fad
lotus-eaten <melikeroblox8101@gmail.com>
2026-09-17 14:50:57 -0700

    Update prompts.lua

--- prompts.lua
+++ prompts.lua
@@ -8,6 +8,11 @@ return {
     "You examine the plushie closely. Its surface is covered in soft fabric, with the characteristic appearance of a slime creature.",
     "You squeeze the plushie for a moment. It's soft, yielding, and oddly satisfying to hold. No melting!"
   },
+  plushie_skeleton = {
+    "You can feel the words leaving your throat with force... 'S-Sasuga!'         ...Huh?",
+    "A 1000 year plan. His gaze burns with the wisdom of infallible strategy, near omniscience.",
+    "Are you attracted to bones?"
+  },
   sword_plushie = {
     "Light, yet firm. Unfortunately, nothing can be done about the weight imbalance in this state. Still good for a pillow fight, though!", 
     "*Shkriiiip!* Despite the prehensile appearance of the sword tassel gripping the scabbard, it's the sound of two velcro patches tearing that tells you the blade is released.",
